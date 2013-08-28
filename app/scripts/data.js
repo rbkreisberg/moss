11f5860537dc870817284f7c4dfe7664453306c1
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-08-27 17:16:20 -0700

    use new data file without certain features

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -8,7 +8,7 @@ define(['queue'], function () {
 
 function getClinicalData (deferral) {
 
-	d3.tsv('data/sampleMeta.tsv')
+	d3.tsv('data/samples.tsv')
 	.row(function(d) {
 		d["N:CLIN:Gestational_Age_at_Delivery"] = +d["N:CLIN:Gestational_Age_at_Delivery"];
 		d["C:QCTL:PL_VERSION"] = +d["C:QCTL:PL_VERSION"];
