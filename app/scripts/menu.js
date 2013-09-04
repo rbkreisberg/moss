e848648d0d3d55d7ac39c915ab5b780d2e259aaf
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-09-04 13:33:49 -0700

    fixed autocomplete to keep list open

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,13 +1,25 @@
 /* jquery and jqueryUI are global */
-define([], function () {
+define(['mediator-js'], function (Mediator) {
     'use strict';
 
     var deferral;
+    var selected;
 
 /* widget creation */
 
     function setupSideMenu() {
-      
+      $("#x_feature_select").on('click', function(evt, ui) {
+        var candidateFeature = $('#feature_search').val();
+        Mediator.publish('application:feature:selected', { label: candidateFeature, axis: 'x'})
+      });
+      $("#y_feature_select").on('click', function(evt, ui) {
+        var candidateFeature = $('#feature_search').val();
+        Mediator.publish('application:feature:selected', { label: candidateFeature, axis: 'y'})
+      });
+      $("#color_feature_select").on('click', function(evt, ui) {
+        var candidateFeature = $('#feature_search').val();
+        Mediator.publish('application:feature:selected', { label: candidateFeature, axis: 'class'})
+      });
     }
 
 /* widget event handling */
@@ -17,23 +29,36 @@ define([], function () {
     }
 
 /* populate widgets with data */
-    function populateAutocomplete( list ) {
-        $("#feature_search").autocomplete({
+    function populateAutocomplete( list ) {      
+
+        var $featureSearch = $("#feature_search").autocomplete({
                                 source : list,
                                 minLength : 2,
                                 delay : 200,
                                 position : { my: "left top", at: "left bottom", of: "#feature_list", collision: "none"},
                                 autoFocus : false,
-                                appendTo : "#feature_list_hidden",
-                                open : function( event, ui) {
-                                    $('#feature_list').empty()
-                                    $('#feature_list_hidden ul').clone().appendTo("#feature_list");
-                                    return true;
+                                appendTo : "#feature_list",
+                                select: function( event, ui ) {
+                                    selected = true;
                                 }
-        })
-        .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-          return $( "<li class=\"feature\">" ).append( "<a>" + item.label + "</a>" ).appendTo( ul );
-        }
+        });
+        
+
+    (function(){
+        
+        $featureSearch.data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+          return $( "<li class=\"feature\">" ).append( "<a class=\"glyphicon glyphicon-list\">  " + item.label + "</a>" ).appendTo( ul );
+        };
+
+       var originalCloseMethod = $featureSearch.data("ui-autocomplete").close;
+        $featureSearch.data("ui-autocomplete").close = function(event) {
+            if (!selected){
+                //close requested by someone else, let it pass
+                originalCloseMethod.apply( this, arguments );
+            }
+            // selected = false;
+        };
+    })();
     }
 
     var Menu = {
